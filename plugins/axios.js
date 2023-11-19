7459611f7d9afca2b778c2668037f6e6ef5f08f0
noatudor <143794128+noatudor@users.noreply.github.com>
2023-11-19 03:11:20 +0100

    Fixed Api Route

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,7 +3,7 @@ import axios from "axios"
 export default defineNuxtPlugin((NuxtApp) => {
 
     axios.defaults.withCredentials = true;
-    axios.defaults.baseURL = 'https://localhost'
+    axios.defaults.baseURL = 'https://api.fikfuck.de'
 
     return {
         provide: { 
