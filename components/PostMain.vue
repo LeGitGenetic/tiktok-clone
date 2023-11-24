673a3f0720ec2468131ad0655d0d0e1a9108025b
noatudor <143794128+noatudor@users.noreply.github.com>
2023-11-24 18:35:52 +0100

    Fixed Ios Problem

--- components/PostMain.vue
+++ components/PostMain.vue
@@ -1,79 +1,61 @@
 <template>
   <div>
     <div @scroll="onScroll" :id="`PostMain-${post.id}`" class="flex border-b py-6">
-        <div @click="isLoggedIn(post.user)" class="cursor-pointer">
+      <!-- User interaction elements -->
+      <div @click="isLoggedIn(post.user)" class="cursor-pointer"></div>
+      <div class="pl-3 w-full px-4">
+        <!-- Post header with Follow button -->
+        <div class="flex items-center justify-between pb-0.5">
+          <button @click="isLoggedIn(post.user)">
+            <span class="font-bold hover:underline cursor-pointer">x</span>
+            <span class="text-[13px] text-light text-gray-500 pl-1 cursor-pointer">xw</span>
+          </button>
+          <button class="border text-[15px] px-[21px] py-0.5 border-[#F02C56] text-[#F02C56] hover:bg-[#ffeef2] font-semibold rounded-md">
+            Follow
+          </button>
         </div>
-        <div class="pl-3 w-full px-4">
-            <div class="flex items-center justify-between pb-0.5">
-                <button @click="isLoggedIn(post.user)">
-                    <span class="font-bold hover:underline cursor-pointer">
-x                    </span>
-                    <span class="text-[13px] text-light text-gray-500 pl-1 cursor-pointer">
-xw                    </span>
-                </button>
-
-                <button class="border text-[15px] px-[21px] py-0.5 border-[#F02C56] text-[#F02C56] hover:bg-[#ffeef2] font-semibold rounded-md">
-                    Follow
-                </button>
-            </div>
-            <div class="text-[14px] text-gray-500 pb-0.5">#fun #cool #SuperAwesome</div>
-            <div class="text-[14px] pb-0.5 flex items-center font-semibold">
-                <Icon name="mdi:music" size="17"/>
-                <div class="px-1">original sound - AWESOME</div>
-                <Icon name="mdi:heart" size="20"/>
-            </div>
-
-            <div class="mt-2.5 flex">
-                <div
-                    @click="displayPost(post)"
-                    class="relative min-h-[480px] max-h-[580px] max-w-[260px] flex items-center bg-black rounded-xl cursor-pointer"
-                >
-                    <video
-                        v-if="post.video"
-                        ref="video"
-                        loop
-                        class="rounded-xl object-cover mx-auto h-full"
-                        :src="post.video"
-                    />
-                    <img
-                        class="absolute right-2 bottom-14"
-                        width="90"
-                        src="~/assets/images/tiktok-logo-white.png"
-                    >
-                </div>
-                <div class="relative mr-[75px]">
-                    <div class="absolute bottom-0 pl-2">
-                        <div class="pb-4 text-center">
-                            <button
-                                @click="isLiked ? unlikePost(post) : likePost(post)"
-                                class="rounded-full bg-gray-200 p-2 cursor-pointer"
-                            >
-                                <Icon
-                                    name="mdi:heart"
-                                    size="25"
-                                    :color="isLiked ? '#F02C56' : ''"
-                                />
-                            </button>
-                            <span class="text-xs text-gray-800 font-semibold">{{ post.likes.length }}</span>
-                        </div>
-
-                        <div class="pb-4 text-center">
-                            <div class="rounded-full bg-gray-200 p-2 cursor-pointer">
-                                <Icon name="bx:bxs-message-rounded-dots" size="25"/>
-                            </div>
-                            <span class="text-xs text-gray-800 font-semibold">43</span>
-                        </div>
-
-                        <div class="text-center">
-                            <div class="rounded-full bg-gray-200 p-2 cursor-pointer">
-                                <Icon name="ri:share-forward-fill" size="25"/>
-                            </div>
-                            <span class="text-xs text-gray-800 font-semibold">55</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <!-- Post tags -->
+        <div class="text-[14px] text-gray-500 pb-0.5">#fun #cool #SuperAwesome</div>
+        <!-- Music and like information -->
+        <div class="text-[14px] pb-0.5 flex items-center font-semibold">
+          <Icon name="mdi:music" size="17"/>
+          <div class="px-1">original sound - AWESOME</div>
+          <Icon name="mdi:heart" size="20"/>
         </div>
+        <!-- Video and interaction section -->
+        <div class="mt-2.5 flex">
+          <div
+              @click="displayPost(post)"
+              class="relative min-h-[480px] max-h-[580px] max-w-[260px] flex items-center bg-black rounded-xl cursor-pointer"
+          >
+            <video
+                v-if="post.video"
+                ref="video"
+                loop
+                class="rounded-xl object-cover mx-auto h-full"
+                :src="post.video"
+                @play="isPlaying = true"
+                @pause="isPlaying = false"
+            />
+            <img
+                class="absolute right-2 bottom-14"
+                width="90"
+                src="~/assets/images/tiktok-logo-white.png"
+            />
+            <button
+                v-if="!isPlaying"
+                class="play-button"
+                @click="togglePlay"
+            >
+              Play
+            </button>
+          </div>
+          <!-- Post actions (like, comment, share) -->
+          <div class="relative mr-[75px]">
+            <!-- ... Like, Comment, Share buttons ... -->
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -113,6 +95,17 @@ onMounted(() => {
   observer.observe(document.getElementById(`PostMain-${post.value.id}`));
 });
 
+
+let isPlaying = ref(false);
+
+
+const togglePlay = () => {
+  if (video.value.paused) {
+    video.value.play();
+  } else {
+    video.value.pause();
+  }
+};
 
 
 onBeforeUnmount(() => {
